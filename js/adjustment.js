6987aaede02108bdd4b5083eeefc8e8543ac65a2
-mxb- <mxb.atria@gmail.com>
2014-07-07 15:21:43 -0700

    revision 2

--- js/adjustment.js
+++ js/adjustment.js
@@ -1,32 +1,90 @@
 var width = document.body.clientWidth;
 var height = document.body.clientHeight;
 
-document.getElementById("announcement").style.marginTop = height * 0.4;
-
-if(width < 560) {
-	document.getElementById("main").style.fontSize = "210%";
+if(width < 640) {
+	document.getElementById("main").style.fontSize = "280%";
 	document.getElementById("supMain").style.fontSize = "110%";
+	document.getElementById("supMain").style.marginTop = "30px";
+	document.getElementById("assetTxt").style.fontSize = "30px";
+	document.getElementById("workTxt").style.fontSize = "30px";
 }
 
-if(width < 480) {
+if(width < 540) {
 	document.getElementById("main").style.fontSize = "165%";
 	document.getElementById("supMain").style.fontSize = "90%";
+	document.getElementById("supMain").style.marginTop = "21px";
+	document.getElementById("assetTxt").style.fontSize = "30px";
+	document.getElementById("workTxt").style.fontSize = "30px";
+	
+	document.getElementById("app1").style.display = "table";
+	document.getElementById("app1").style.marginLeft = "auto";
+	document.getElementById("app1").style.marginRight = "auto";
+	
+	document.getElementById("app2").style.display = "table";
+	document.getElementById("app2").style.marginLeft = "auto";
+	document.getElementById("app2").style.marginRight = "auto";
+	
+	document.getElementById("app3").style.display = "table";
+	document.getElementById("app3").style.marginLeft = "auto";
+	document.getElementById("app3").style.marginRight = "auto";
+	
+	document.getElementById("app4").style.display = "table";
+	document.getElementById("app4").style.marginLeft = "auto";
+	document.getElementById("app4").style.marginRight = "auto";
 }
 
 window.onresize = function(event) {
 	var width = document.body.clientWidth;
 	
-	if(width < 560) {
-		document.getElementById("main").style.fontSize = "210%";
+	if(width < 640) {
+		document.getElementById("main").style.fontSize = "280%";
 		document.getElementById("supMain").style.fontSize = "110%";
-	} else if(width >= 560) {
-		document.getElementById("main").style.fontSize = "300%";
+		document.getElementById("supMain").style.marginTop = "30px";
+		document.getElementById("assetTxt").style.fontSize = "30px";
+		document.getElementById("workTxt").style.fontSize = "30px";
+	
+	} else if(width >= 640) {
+		document.getElementById("main").style.fontSize = "375%";
 		document.getElementById("supMain").style.fontSize = "120%";
+		document.getElementById("supMain").style.marginTop = "35px";
+		document.getElementById("assetTxt").style.fontSize = "60px";
+		document.getElementById("workTxt").style.fontSize = "60px";
 	}
 	
-	if(width < 480) {
+	if(width < 540) {
 		document.getElementById("main").style.fontSize = "165%";
 		document.getElementById("supMain").style.fontSize = "90%";
+		document.getElementById("supMain").style.marginTop = "21px";
+		document.getElementById("assetTxt").style.fontSize = "30px";
+		document.getElementById("workTxt").style.fontSize = "30px";
+		
+		document.getElementById("app1").style.display = "table";
+		document.getElementById("app1").style.marginLeft = "auto";
+		document.getElementById("app1").style.marginRight = "auto";
+	
+		document.getElementById("app2").style.display = "table";
+		document.getElementById("app2").style.marginLeft = "auto";
+		document.getElementById("app2").style.marginRight = "auto";
+	
+		document.getElementById("app3").style.display = "table";
+		document.getElementById("app3").style.marginLeft = "auto";
+		document.getElementById("app3").style.marginRight = "auto";
+	
+		document.getElementById("app4").style.display = "table";
+		document.getElementById("app4").style.marginLeft = "auto";
+		document.getElementById("app4").style.marginRight = "auto";
+	} else {
+		document.getElementById("app1").style.display = "inline";
+		document.getElementById("app1").style.margin = "10px";
+	
+		document.getElementById("app2").style.display = "inline";
+		document.getElementById("app2").style.margin = "10px";
+	
+		document.getElementById("app3").style.display = "inline";
+		document.getElementById("app3").style.margin = "10px";
+	
+		document.getElementById("app4").style.display = "inline";
+		document.getElementById("app4").style.margin = "10px";
 	}
 	
 };
